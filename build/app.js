ff664fbb336034e918fd2dbd8298f68d1990c51f
Jose Ruben Maldonado Herrera <rub.maler.22@gmail.com>
2021-12-27 18:44:30 -0600

    Generate choc bar html template

--- build/app.js
+++ build/app.js
@@ -1,6 +1,6 @@
 "use strict";
-var chocolateBarHeight = 0;
-var chocolateBarWidth = 0;
+let chocolateBarHeight = 0;
+let chocolateBarWidth = 0;
 window.onload = () => {
     const inputForm = document.getElementById("set-size-bar");
     inputForm === null || inputForm === void 0 ? void 0 : inputForm.addEventListener("submit", (event) => {
@@ -9,10 +9,22 @@ window.onload = () => {
         let height = form[0].value;
         let width = form[1].value;
         this.setChocolateBarSize(height, width);
-        console.log(this.chocolateBarWidth);
+        this.deployChocolateBar();
     });
 };
 function setChocolateBarSize(height, width) {
     this.chocolateBarHeight = height;
     this.chocolateBarWidth = width;
+}
+function deployChocolateBar() {
+    let chocBarDiv = document.getElementById("cbd");
+    let rows = "";
+    for (let i = 0; i < this.chocolateBarHeight; i++) {
+        rows += "<div class='row'></div>";
+    }
+    if (chocBarDiv !== null) {
+        for (let i = 0; i < this.chocolateBarWidth; i++) {
+            chocBarDiv.innerHTML += "<div class='col'>" + rows + "</div>";
+        }
+    }
 }
--- src/scripts/app.ts
+++ src/scripts/app.ts
@@ -1,5 +1,5 @@
-var chocolateBarHeight: number = 0;
-var chocolateBarWidth: number = 0;
+let chocolateBarHeight: number = 0;
+let chocolateBarWidth: number = 0;
 
 window.onload = () => {
   const inputForm = document.getElementById("set-size-bar");
@@ -9,10 +9,24 @@ window.onload = () => {
     let height: number = form[0].value;
     let width: number = form[1].value;
     this.setChocolateBarSize(height, width);
+    this.deployChocolateBar();
   });
 };
 
 function setChocolateBarSize(height: number, width: number): void {
   this.chocolateBarHeight = height;
   this.chocolateBarWidth = width;
+}
+
+function deployChocolateBar(): void {
+  let chocBarDiv = document.getElementById("cbd");
+  let rows = "";
+  for (let i = 0; i < this.chocolateBarHeight; i++) {
+    rows += "<div class='row'></div>";
+  }
+  if (chocBarDiv !== null) {
+    for (let i = 0; i < this.chocolateBarWidth; i++) {
+      chocBarDiv.innerHTML += "<div class='col'>" + rows + "</div>";
+    }
+  }
 }
