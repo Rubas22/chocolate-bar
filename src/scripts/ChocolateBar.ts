880fe6f6b477f2f82ed8761844ccec5d529604f3
Jose Ruben Maldonado Herrera <rub.maler.22@gmail.com>
2022-02-12 14:46:14 -0600

    Document chocolate bar class

--- src/scripts/ChocolateBar.ts
+++ src/scripts/ChocolateBar.ts
@@ -1,6 +1,20 @@
+/**
+ * Chocolate bar object. here lies the algorithm to calculate minimum cost
+ *
+ * @class {ChocolateBar}
+ * @implements {iChocolateBar}
+ */
 class ChocolateBar implements iChocolateBar {
   rowEdges: Edge[];
   colEdges: Edge[];
+  /**
+   * The weights of the rows and colums are given as parameters.
+   * Those weights are mapped into {@rowEdges} and {@colEdges} indicating the orientation.
+   *
+   * @constructs
+   * @param rowEdgesWeights -
+   * @param colEdgesWeights
+   */
   constructor(rowEdgesWeights: number[] = [], colEdgesWeights: number[] = []) {
     this.rowEdges = rowEdgesWeights.map((rowEdgeWeight) => {
       return new Edge(rowEdgeWeight, "row");
@@ -10,10 +24,28 @@ class ChocolateBar implements iChocolateBar {
     });
   }
 
+  /**
+   * Height value is based on the {rowEdges} lenght
+   */
   get height(): number {
     return this.rowEdges.length + 1;
   }
 
+  /**
+   * Width value is based on the {colEdges} lenght
+   */
+  get width(): number {
+    return this.colEdges.length + 1;
+  }
+
+  /**
+   * Minimum Cost Algorithm
+   *
+   * @description Edges are cut from largest to lowest according to weight.
+   * The weight of an edge is added to the cost when the edge is cut.
+   * Weight must be added as many time as number of slices the are in the opposite orientation.
+   * Number of slices are tracked in count variables.
+   */
   get minimumCost(): number {
     let minimumCost = 0;
     let rowsCount = 1;
@@ -33,12 +65,11 @@ class ChocolateBar implements iChocolateBar {
     return minimumCost;
   }
 
+  /**
+   * All edges are sorted from max weight to min weight
+   */
   get sortedEdges(): Edge[] {
     const edges = [...this.rowEdges, ...this.colEdges];
     return edges.sort((edgeA, edgeB) => edgeB.weight - edgeA.weight);
-  }
-
-  get width(): number {
-    return this.colEdges.length + 1;
   }
 }
