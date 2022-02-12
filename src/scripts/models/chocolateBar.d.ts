116b906bc6e4a73b4e4e01b45c143c3333f6ae14
Jose Ruben Maldonado Herrera <rub.maler.22@gmail.com>
2022-02-12 13:52:17 -0600

    Document chocolate bar interface

--- src/scripts/models/chocolateBar.d.ts
+++ src/scripts/models/chocolateBar.d.ts
@@ -1,8 +1,31 @@
+/**
+ * Chocolate bar interface
+ * 
+ * @interface {iChocolateBar} 
+ */
 interface iChocolateBar {
+  /**
+   * Array with the row edges
+   */
   rowEdges: iEdge[];
+  /**
+   * Array with the column edges
+   */
   colEdges: iEdge[];
+  /**
+   * Height of the chocolate bar
+   */
   get height(): number;
+  /**
+   * Width of the chocolate bar 
+   */
   get width(): number;
+  /**
+   * The minimum cost of all posibles ways of cutting the chocolate bar
+   */
   get minimumCost(): number;
+  /**
+   * An array with all the edges sorted by weight
+   */
   get sortedEdges(): iEdge[];
 }
