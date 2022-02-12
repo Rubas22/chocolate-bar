8b40105b242b9a86968de4352391bc8ad9a2cc48
Jose Ruben Maldonado Herrera <rub.maler.22@gmail.com>
2022-02-12 13:51:59 -0600

    Document edge interface

--- src/scripts/models/edge.d.ts
+++ src/scripts/models/edge.d.ts
@@ -1,6 +1,22 @@
+/**
+ * Two posible orientations for edges
+ *
+ * @typedef {string} Orientation
+ */
 type Orientation = "row" | "col";
 
+/**
+ * Edge interface
+ *
+ * @interface {iEdge}
+ */
 interface iEdge {
+  /**
+   * Value of the edge weight
+   */
   weight: number;
+  /**
+   * Wheter the edge belongs to a row or a column
+   */
   orientation: Orientation;
 }
