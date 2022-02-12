f19e5aeaa8f897861266868c9f8157bb24957775
Jose Ruben Maldonado Herrera <rub.maler.22@gmail.com>
2022-02-12 14:45:29 -0600

    Document edge class

--- src/scripts/Edge.ts
+++ src/scripts/Edge.ts
@@ -1,6 +1,19 @@
+/**
+ * Edge object template
+ * 
+ * @class {Edge}
+ * @implements {iEdge}
+ */
 class Edge implements iEdge {
   weight: number;
   orientation: Orientation;
+  /**
+   * The properties {@weight} and {@orientation} are directly input 
+   * 
+   * @construcs
+   * @param weight - Value of the edge weight
+   * @param orientation - Value of the orientation
+   */
   constructor(weight: number, orientation: Orientation) {
     this.weight = weight;
     this.orientation = orientation;
